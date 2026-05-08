acfd44215004e7fb47d48e9a1574a1cc9d9788af
cyptonik <enixydex15@gmail.com>
2026-05-08 19:16:51 +0300

    removed unnecessary annotation

--- src/main/java/org/cloud/storage/ServletInitializer.java
+++ src/main/java/org/cloud/storage/ServletInitializer.java
@@ -1,12 +1,10 @@
 package org.cloud.storage;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@NotNull
     @Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
