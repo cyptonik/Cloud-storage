2428da0dfc9025d2ff508553b8c6b499054fd755
cyptonik <enixydex15@gmail.com>
2026-05-08 19:17:18 +0300

    created first version of /api/resource endpoint

--- src/main/java/org/cloud/storage/controller/ResourceController.java
+++ src/main/java/org/cloud/storage/controller/ResourceController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.util.Arrays;
 
 @RestController
 public class ResourceController {
@@ -27,6 +29,65 @@ public class ResourceController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid path");
         }
 
-        return null;
+        try {
+            S3ResourceDto file = findFile(path);
+            S3ResourceDto folder = findFolder(path);
+            if (file == null && folder == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "not found");
+            }
+            return (file != null) ? file : folder;
+        } catch (S3Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.awsErrorDetails().errorMessage());
+        }
+    }
+
+    private S3ResourceDto findFolder(String path) throws S3Exception {
+        String prefix = findS3Prefix(path);
+        if (prefix == null) {
+            return null;
+        }
+
+        if (!path.endsWith("/")) {
+            return null;
+        }
+
+        String folderName = path.equals("/") ? path : Arrays.stream(path.split("/")).toList().getLast();
+        return new S3ResourceDto(prefix, folderName, null, "DIRECTORY");
+    }
+
+    private S3ResourceDto findFile(String path) throws S3Exception {
+        S3Object fileObject = findS3Object(path);
+        if (fileObject == null) {
+            return null;
+        }
+
+        if (path.endsWith("/") || path.isEmpty()) {
+            return null;
+        }
+
+        String fileName = path.contains("/") ? path.substring(path.lastIndexOf("/") + 1) : path;
+
+        String exactPath = path.length() - fileName.length() > 0 ? path.substring(0, path.length() - fileName.length()) : "/";
+        return new S3ResourceDto(exactPath, fileName, fileObject.size(), "FILE");
+    }
+
+    private S3Object findS3Object(String path) {
+        ListObjectsV2Response res = s3Client.listObjectsV2(ListObjectsV2Request.builder()
+                .bucket(bucket)
+                .prefix(path)
+                .maxKeys(1)
+                .build());
+
+        return res.contents().stream().filter(obj -> obj.key().equals(path)).findFirst().orElse(null);
+    }
+
+    private String findS3Prefix(String path) {
+        ListObjectsV2Response res = s3Client.listObjectsV2(ListObjectsV2Request.builder()
+                .bucket(bucket)
+                .prefix(path)
+                .maxKeys(1)
+                .build());
+
+        return res.prefix();
     }
 }
