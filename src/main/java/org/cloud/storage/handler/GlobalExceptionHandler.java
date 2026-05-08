40b387dc4042499b2403ce98d5ba3c18c856ccae
cyptonik <enixydex15@gmail.com>
2026-05-09 00:21:43 +0300

    temporary updated handler to catch all exceptions

--- src/main/java/org/cloud/storage/handler/GlobalExceptionHandler.java
+++ src/main/java/org/cloud/storage/handler/GlobalExceptionHandler.java
@@ -3,12 +3,11 @@ package org.cloud.storage.handler;
 import org.cloud.storage.dto.ErrorDto;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResponseStatusException.class)
+    @ExceptionHandler(Exception.class)
     public ErrorDto error(Exception e) {
         return new ErrorDto(e.getMessage());
     }
