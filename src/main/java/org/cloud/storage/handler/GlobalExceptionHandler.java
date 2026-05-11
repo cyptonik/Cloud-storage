458e5934816322730cc6dfcdbcad2273c0a4da7e
cyptonik <enixydex15@gmail.com>
2026-05-12 02:15:29 +0300

    updated global exception handler

--- src/main/java/org/cloud/storage/handler/GlobalExceptionHandler.java
+++ src/main/java/org/cloud/storage/handler/GlobalExceptionHandler.java
@@ -1,14 +1,42 @@
 package org.cloud.storage.handler;
 
 import org.cloud.storage.dto.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorDto> handleResponseStatus(ResponseStatusException e) {
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(new ErrorDto(e.getReason()));
+    }
+
+    @ExceptionHandler(S3Exception.class)
+    public ResponseEntity<ErrorDto> handleS3(S3Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorDto(e.awsErrorDetails().errorMessage()));
+    }
+
+    @ExceptionHandler(NoSuchKeyException.class)
+    public ResponseEntity<ErrorDto> handleNoKey(S3Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorDto(e.awsErrorDetails().errorMessage()));
+    }
+
     @ExceptionHandler(Exception.class)
-    public ErrorDto error(Exception e) {
-        return new ErrorDto(e.getMessage());
+    public ResponseEntity<ErrorDto> handleGeneral(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorDto(e.getMessage()));
     }
 }
